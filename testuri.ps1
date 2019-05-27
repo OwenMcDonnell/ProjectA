82575edb2ce1dc1d1aa897501fc096518e1f9b0c
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2019-05-27 09:12:25 -0700

    Update testuri.ps1

--- testuri.ps1
+++ testuri.ps1
@@ -1,6 +1,6 @@
 
 $port = 3005
 
-$url = "http://localhost:$port"    
+$url = [System.Uri]"http://localhost:$port"    
 Write-Host "Checking url: $url"
 $result = Invoke-WebRequest -Uri $url -Method HEAD 
